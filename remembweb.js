d6fd2e10cea75ee476769f17d51a7f90be0cf9fd
Janin Grandne <47568974+dabizi@users.noreply.github.com>
2019-12-04 00:09:24 +0100

    Update remembweb.js

--- remembweb.js
+++ remembweb.js
@@ -1,1 +1,13 @@
 document.body.style.border = "5px solid red";
+
+chrome.runtime.onMessage.addListener(gotMessage);
+
+function gotMessage(request, sender, sendResponse) {
+ console.log(message.txt); 
+  if (message.txt === "hello") {
+   let paragraphs = document.getElementsByTagName('p');
+    for (elt of paragraphs) {
+     elt.style['background-color'] = '#FF00FF'; 
+    }
+  }
+}
