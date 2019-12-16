711b55fe104ff1c38ae5cbb119e3677c1c894ea2
Janin Grandne <47568974+dabizi@users.noreply.github.com>
2019-12-16 16:45:34 +0100

    Add files via upload

--- background.js
+++ background.js
@@ -4,6 +4,13 @@ chrome.runtime.onMessage.addListener(receiver);
 
 window.word = "Please highlight what you want to save";
 
+var contextMenuItem = {
+                    "id": "iconid",
+                    "title": "Save image",
+                    "contexts": ["all"]  // type of context
+             };
+             chrome.contextMenus.create(contextMenuItem);
+
 function receiver(request, sender, sendResponse) {
   console.log(request);
   word = request.text;
